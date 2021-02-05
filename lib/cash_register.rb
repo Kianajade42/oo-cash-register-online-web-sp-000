abc0026329631d4324b424502781a026f0f7889e
Kiana Long <kianajade42.kl@gmail.com>
2021-02-05 20:11:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,17 @@
 class CashRegister
 
-  attr_accessor :new, :discount, :total, :last_transaction
+  attr_accessor :items, :discount, :total, :last_transaction
 
   def initialize(discount=0)
-    @discount = discount
-    @new = []
     @total = 0
+    @discount = discount
+    @items = []
   end
+
   def add_item(title, amount, quantity=1)
     self.total += amount * quantity
     quantity.times do
+      items << title
     end
     self.last_transaction = amount * quantity
   end
